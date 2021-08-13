31a50132f92ebcfb46d37fd77268bdd0122ab410
Aswin V <vaswin91@gmail.com>
2021-08-13 18:49:26 +0530

    PageHeader component

---
+++ shortly/components/PageHeader/__tests__/index.tsx
@@ -0,0 +1,30 @@
+import { screen, render, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import PageHeader from '..'
+
+beforeEach(() => {
+  cleanup()
+  global.innerWidth = 500
+
+  global.dispatchEvent(new Event('resize'))
+})
+
+test('menu button activates the mobile navigation', async () => {
+  render(<PageHeader />)
+
+  userEvent.click(screen.getByLabelText('Menu'))
+
+  screen.debug()
+
+  expect(screen.getByRole('list')).toHaveClass('nav__list--active')
+})
+
+test('menu button toggle closes the mobile navigation', async () => {
+  render(<PageHeader />)
+
+  userEvent.dblClick(screen.getByLabelText('Menu'))
+
+  screen.debug()
+
+  expect(screen.getByRole('list')).not.toHaveClass('nav__list--active')
+})
--- shortly/components/PageHeader/index.tsx
+++ shortly/components/PageHeader/index.tsx
@@ -1,19 +1,48 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import Logo from 'components/Logo'
+import styles from './index.module.css'
 
 export default function PageHeader(): JSX.Element {
+  const [isMenuOpen, setState] = useState(false)
+
+  function toggle() {
+    setState((prevState) => !prevState)
+  }
+
   return (
-    <header>
-      <nav aria-label="Main Navigation">
-        <Link href="/">
-          <a>
-            <Logo theme="dark" aria-hidden="true" />
-          </a>
-        </Link>
-        <ul>
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>Resources</li>
+    <header className={styles['header']}>
+      <Link href="/">
+        <a aria-label="home" title="home" className={styles['header__logo']}>
+          <Logo theme="dark" aria-hidden="true" role="img" />
+        </a>
+      </Link>
+      <nav
+        aria-label="Main Navigation"
+        className={styles['nav']}
+        role="navigation"
+      >
+        {/* menu button should be placed within <nav>, so it's still reachable by landmark navigation. */}
+        <button
+          aria-label="Menu"
+          className={styles['nav__button']}
+          onClick={toggle}
+          aria-expanded={isMenuOpen}
+          aria-controls="menu-list"
+        >
+          <img src="/menu.svg" alt="Menu" aria-hidden="true"></img>
+        </button>
+        <ul
+          id="menu-list"
+          className={`${styles['nav__list']} ${
+            isMenuOpen ? styles['nav__list--active'] : ''
+          }`}
+        >
+          <li className={styles['nav__list-item']}>Features</li>
+          <li className={styles['nav__list-item']}>Pricing</li>
+          <li className={styles['nav__list-item']}>Resources</li>
+          <li className={styles['nav__list-item']}>Login</li>
+          <li className={styles['nav__list-item']}>Sign Up</li>
         </ul>
       </nav>
     </header>
