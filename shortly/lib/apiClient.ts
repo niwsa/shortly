5ba8eb0657977071f6783d09fb9f9906c0c645c0
Aswin V <vaswin91@gmail.com>
2021-08-14 17:34:04 +0530

    apiClient for <Form>

---
+++ shortly/lib/apiClient.ts
@@ -0,0 +1,40 @@
+export type ShrtCodeDataRes = {
+  ok: boolean
+  result: ShrtCodeData
+}
+
+export type ShrtCodeData = {
+  code: string
+  short_link: string
+  full_short_link: string
+  short_link2: string
+  full_short_link2: string
+  share_link: string
+  full_share_link: string
+  original_link: string
+}
+
+
+export type ShrtCodeErr = {
+  ok: boolean,
+  error_code: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+  error: string
+}
+
+
+
+async function client(path: string): Promise<ShrtCodeDataRes | ShrtCodeErr> {
+  const url = process.env.NEXT_PUBLIC_API_BASE + path
+  const response = await fetch(url)
+
+  const data: ShrtCodeDataRes | ShrtCodeErr = await response.json()
+  // console.log(data)
+  if (response.ok) {
+    return data
+  } else {
+    return Promise.reject(data)
+  }
+
+}
+
+export default client
