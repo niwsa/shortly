32b79ece41061fa9ac4043294f6b9591a2ee2eab
Aswin V <vaswin91@gmail.com>
2021-08-15 01:36:59 +0530

    - Spacer item in nav list - Handle window resize when menu is open - CSS refactor

--- shortly/components/PageHeader/index.tsx
+++ shortly/components/PageHeader/index.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Logo from 'components/Logo'
 import styles from './index.module.css'
+import useWindowSize from 'hooks/useWindowSize'
 
 export default function PageHeader(): JSX.Element {
-  const [isMenuOpen, setState] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   function toggle() {
-    setState((prevState) => !prevState)
+    setIsMenuOpen((prevState) => !prevState)
   }
+
+  const [width] = useWindowSize()
+
+  useEffect(() => {
+    // If user resizes the browser window (or any event which causes window to resize ),
+    // close the menu
+    if (width > 700) {
+      setIsMenuOpen(false)
+    }
+  }, [width])
 
   return (
     <header className={styles['header']}>
@@ -22,7 +33,10 @@ export default function PageHeader(): JSX.Element {
         className={styles['nav']}
         role="navigation"
       >
-        {/* menu button should be placed within <nav>, so it's still reachable by landmark navigation. */}
+        {/* menu button should be placed within <nav>, 
+        so it's still reachable by landmark navigation. 
+        👉 https://www.a11ymatters.com/pattern/mobile-nav/
+        */}
         <button
           aria-label="Menu"
           className={styles['nav__button']}
@@ -41,6 +55,9 @@ export default function PageHeader(): JSX.Element {
           <li className={styles['nav__list-item']}>Features</li>
           <li className={styles['nav__list-item']}>Pricing</li>
           <li className={styles['nav__list-item']}>Resources</li>
+          <li
+            className={`${styles['nav__list-item']} ${styles['nav__list-item--spacer']}`}
+          ></li>
           <li className={styles['nav__list-item']}>Login</li>
           <li className={styles['nav__list-item']}>Sign Up</li>
         </ul>
