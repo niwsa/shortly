972f2ac80c72248faac331a3fec182b464986efa
Aswin V <vaswin91@gmail.com>
2021-08-14 17:31:21 +0530

    jest config changes - .js -> .ts - setupTests for loading nextjs env

--- shortly/jest.config.ts
+++ shortly/jest.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   roots: ['<rootDir>'],
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
@@ -15,9 +17,15 @@ module.exports = {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
     '^components/(.*)$': '<rootDir>/components/$1',
+    '^lib/(.*)$': '<rootDir>/lib/$1',
   },
   // Runs special logic, such as cleaning up components
   // when using React Testing Library and adds special
   // extended assertions to Jest
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+  ],
+  globalSetup: '<rootDir>/setupTests.ts'
 }
+
+export default config
