413d92f207c0de5bd7f89151cf166fcf94fb076a
Aswin V <vaswin91@gmail.com>
2021-08-14 17:25:11 +0530

    Remove initial test files

--- shortly/__tests__/hello.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen, cleanup } from '@testing-library/react'
-import Hello from '../components/hello'
-
-afterEach(cleanup)
-test('Test that component renders', () => {
-  const message = 'Tomorrow'
-  render(<Hello message={message} />)
-  screen.getByText(message)
-})
--- shortly/components/hello.tsx
+++
@@ -1,7 +0,0 @@
-type HelloProps = {
-  message: string
-}
-
-const Hello = ({ message }: HelloProps): JSX.Element => <div>{message}</div>
-
-export default Hello
