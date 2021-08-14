f16032012536fd19645897a0b73302573b48cd18
Aswin V <vaswin91@gmail.com>
2021-08-15 01:35:40 +0530

    Add a new test for mobile nav close on window resize

--- shortly/components/PageHeader/__tests__/index.tsx
+++ shortly/components/PageHeader/__tests__/index.tsx
@@ -1,13 +1,25 @@
-import { screen, render } from '@testing-library/react'
+import { screen, render, waitFor, act } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import matchMediaPolyfill from 'mq-polyfill'
 import PageHeader from '..'
 
+beforeAll(() => {
+  matchMediaPolyfill(window)
+  window.resizeTo = function resizeTo(width, height) {
+    Object.assign(this, {
+      innerWidth: width,
+      innerHeight: height,
+      outerWidth: width,
+      outerHeight: height,
+    }).dispatchEvent(new this.Event('resize'))
+  }
+})
 beforeEach(() => {
-  global.innerWidth = 500
-  global.dispatchEvent(new Event('resize'))
+  // resize to smaller than 700px
+  window.resizeTo(500, 900)
 })
 
-test('menu button activates the mobile navigation', async () => {
+test('menu button activates the mobile navigation', () => {
   render(<PageHeader />)
 
   userEvent.click(screen.getByLabelText('Menu'))
@@ -15,10 +27,26 @@ test('menu button activates the mobile navigation', async () => {
   expect(screen.getByRole('list')).toHaveClass('nav__list--active')
 })
 
-test('menu button toggle closes the mobile navigation', async () => {
+test('menu button toggle closes the mobile navigation', () => {
   render(<PageHeader />)
 
   userEvent.dblClick(screen.getByLabelText('Menu'))
 
   expect(screen.getByRole('list')).not.toHaveClass('nav__list--active')
+})
+
+test('opened menu closes when window is resized to more than 700px', async () => {
+  render(<PageHeader />)
+
+  userEvent.click(screen.getByLabelText('Menu'))
+
+  expect(screen.getByRole('list')).toHaveClass('nav__list--active')
+
+  act(() => {
+    window.resizeTo(900, 900)
+  })
+
+  await waitFor(() =>
+    expect(screen.getByRole('list')).not.toHaveClass('nav__list--active')
+  )
 })
