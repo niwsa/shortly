2a9f2f74f44b285730ff9d7dde0444073cd20ac8
Aswin V <vaswin91@gmail.com>
2021-08-14 17:33:38 +0530

    Form Component

---
+++ shortly/components/Form/__tests__/index.tsx
@@ -0,0 +1,52 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import 'whatwg-fetch'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import Form from '..'
+
+const server = setupServer(
+  rest.get(`${process.env.NEXT_PUBLIC_API_BASE}/shorten`, (req, res, ctx) => {
+    const original_link = req.url.searchParams.get('url')
+    return res(
+      ctx.json({
+        ok: true,
+        result: {
+          code: 'stE2dB',
+          short_link: 'shrtco.de/stE2dB',
+          full_short_link: 'https://shrtco.de/stE2dB',
+          short_link2: '9qr.de/stE2dB',
+          full_short_link2: 'https://9qr.de/stE2dB',
+          short_link3: 'shiny.link/stE2dB',
+          full_short_link3: 'https://shiny.link/stE2dB',
+          share_link: 'shrtco.de/share/stE2dB',
+          full_share_link: 'https://shrtco.de/share/stE2dB',
+          original_link,
+        },
+      })
+    )
+  })
+)
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
+
+test('fetches shortUrl on form submit', async () => {
+  const addShrtCodeToList = jest.fn()
+  render(<Form addShrtCodeToList={addShrtCodeToList} />)
+  const urlInput = screen.getByLabelText(/URL to shorten/i)
+  const submitBtn = screen.getByText(/Shorten It!/i)
+  userEvent.type(urlInput, 'https://nextjs.org')
+  userEvent.click(submitBtn)
+  await waitForElementToBeRemoved(() =>
+    screen.getByLabelText(/Fetching short link/i)
+  )
+  await waitFor(() => screen.getByText(/Shorten It!/i))
+  expect(urlInput).toHaveValue('')
+})
---
+++ shortly/components/Form/index.tsx
@@ -0,0 +1,74 @@
+import { FormEvent, useState } from 'react'
+import { ShrtCodeData, ShrtCodeDataRes } from 'lib/apiClient'
+import styles from './index.module.css'
+
+interface FormProps {
+  addShrtCodeToList: (data: ShrtCodeData) => void
+}
+
+const Form = (props: FormProps): JSX.Element => {
+  const { addShrtCodeToList } = props
+  const [inputUrl, setInputUrl] = useState('')
+
+  const handleChange = (e: FormEvent<HTMLInputElement>): void => {
+    setInputUrl(e.currentTarget.value)
+  }
+
+  const [fetchingShrtCode, setFetchingShrtCode] = useState(false)
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault()
+    // show loading state
+    setFetchingShrtCode(true)
+    const client = (await import('lib/apiClient')).default
+    try {
+      const { result } = (await client(
+        `/shorten?url=${inputUrl}`
+      )) as ShrtCodeDataRes
+      addShrtCodeToList(result)
+    } catch (err) {
+      console.error(err)
+    } finally {
+      // remove loading state
+      setFetchingShrtCode(false)
+      // reset input
+      setInputUrl('')
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="url"
+        placeholder="Shorten a link here..."
+        pattern="https://.*"
+        title="Only https urls allowed"
+        required
+        value={inputUrl}
+        onChange={handleChange}
+        aria-label="URL to shorten"
+      ></input>
+      {fetchingShrtCode ? (
+        <div
+          aria-live="polite"
+          aria-label="Fetching short link"
+          role="status"
+          className={styles['form__status']}
+        >
+          <img
+            src="/bars.svg"
+            alt="fetching short link"
+            aria-hidden="true"
+            className={styles['form__spinner']}
+          ></img>
+        </div>
+      ) : (
+        <button type="submit" className={styles['form__submit']}>
+          Shorten It!
+        </button>
+      )}
+    </form>
+  )
+}
+
+export default Form
